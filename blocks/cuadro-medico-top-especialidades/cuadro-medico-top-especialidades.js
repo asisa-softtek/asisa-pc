f47ec2b76cac3c96cd6e6e3bbcf3388422c7ef50
asisa-softtek <jorge.lorenzo@ext.softtek.com>
2026-03-31 06:36:13 +0200

    Update cuadro-medico-top-especialidades.js

--- blocks/cuadro-medico-top-especialidades/cuadro-medico-top-especialidades.js
+++ blocks/cuadro-medico-top-especialidades/cuadro-medico-top-especialidades.js
@@ -6,7 +6,7 @@
  */
 
 function toSlug(str) {
-  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\./g, '').replace(/\s+/g, '-');
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
 }
 
 export default function decorate(block) {
